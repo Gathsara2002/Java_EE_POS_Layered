bd43117f1ce0908df0d5c50da64019c0ea3e1950
Gathsara2002 <liviru2002mmg@gmail.com>
2023-09-15 21:06:04 +0530

    create CrudDAO interface to add common method

---
+++ O16_POS_SPA_Back_End/src/lk/ijse/pos/dao/CrudDAO.java
@@ -0,0 +1,19 @@
+/**
+ * @author : Gathsara
+ * created : 9/15/2023 -- 9:04 PM
+ **/
+
+package lk.ijse.pos.dao;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface CrudDAO<T> {
+    ArrayList<T> getAll() throws SQLException, ClassNotFoundException;
+
+    boolean save(T entity) throws SQLException, ClassNotFoundException;
+
+    boolean update(T entity) throws SQLException, ClassNotFoundException;
+
+    boolean delete(String id) throws SQLException, ClassNotFoundException;
+}
