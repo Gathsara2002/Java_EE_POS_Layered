51016c58421a0f13aba45a7578f7cf92bae0504c
Gathsara2002 <liviru2002mmg@gmail.com>
2023-09-16 17:27:41 +0530

    create OrderDetailsDAO and OrderDetailsDAOImpl

---
+++ O16_POS_SPA_Back_End/src/lk/ijse/pos/dao/custom/OrderDetailsDAO.java
@@ -0,0 +1,12 @@
+/**
+ * @author : Gathsara
+ * created : 9/16/2023 -- 5:26 PM
+ **/
+
+package lk.ijse.pos.dao.custom;
+
+import lk.ijse.pos.dao.CrudDAO;
+import lk.ijse.pos.entity.OrderDetailsEntity;
+
+public interface OrderDetailsDAO extends CrudDAO<OrderDetailsEntity> {
+}
---
+++ O16_POS_SPA_Back_End/src/lk/ijse/pos/dao/custom/impl/OrderDetailsDAOImpl.java
@@ -0,0 +1,35 @@
+/**
+ * @author : Gathsara
+ * created : 9/16/2023 -- 5:27 PM
+ **/
+
+package lk.ijse.pos.dao.custom.impl;
+
+import lk.ijse.pos.dao.custom.OrderDetailsDAO;
+import lk.ijse.pos.entity.OrderDetailsEntity;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class OrderDetailsDAOImpl implements OrderDetailsDAO {
+    @Override
+    public ArrayList<OrderDetailsEntity> getAll(Connection connection) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean save(OrderDetailsEntity entity, Connection connection) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean update(OrderDetailsEntity entity, Connection connection) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String id, Connection connection) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+}
